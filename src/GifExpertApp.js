5b7e6a13df8a1bd817e644ab1d6b912a35278513
Enuel <enuel.monzon@gmail.com>
2022-01-05 04:58:14 -0300

    Lista de categorias

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
-function GifExpertApp() {
+const GifExpertApp = () => {
+  const [categories, setCategories] = useState([
+    "One Punch",
+    "Samurai X",
+    "Dragon Ball",
+  ]);
+
+  const handleAdd = () => {
+    // setCategories([...categories, "Naruto"]);
+    setCategories((cats) => [...cats, "Naruto"]);
+  };
+
   return (
     <>
       <h2>GifExpertApp</h2>
       <hr />
+
+      <button onClick={handleAdd}>Agregar</button>
+
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
     </>
   );
-}
+};
 
 export default GifExpertApp;
